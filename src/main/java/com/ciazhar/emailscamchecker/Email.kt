6bb7b991849696c2d7a7f246c4b93925e6e34534
ciazhar <ciazhar.id@gmail.com>
2018-02-05 15:06:14 +0700

    buat model email

---
+++ src/main/java/com/ciazhar/emailscamchecker/Email.kt
@@ -0,0 +1,8 @@
+package com.ciazhar.emailscamchecker
+
+/**
+ * Created by ciazhar on 02/02/18.
+ * [ Documentatiion Here ]
+ */
+class Email(var email: String? = null, var options: String? = null)
+
