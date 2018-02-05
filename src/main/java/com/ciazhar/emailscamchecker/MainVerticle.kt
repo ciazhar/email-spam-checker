5f91303460a7b93dd8a897edb33b326ea5972d6e
ciazhar <ciazhar.id@gmail.com>
2018-02-05 15:07:09 +0700

    tambah api scoring email

--- src/main/java/com/ciazhar/emailscamchecker/MainVerticle.kt
+++ src/main/java/com/ciazhar/emailscamchecker/MainVerticle.kt
@@ -1,15 +1,48 @@
 package com.ciazhar.emailscamchecker
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.RequestOptions
+import io.vertx.core.json.Json
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.BodyHandler
 
 class MainVerticle : AbstractVerticle() {
 
+    private fun checkScore(ctx : RoutingContext){
+
+        val client = vertx.createHttpClient()
+
+        val mail = Json.decodeValue(ctx.bodyAsString,Email::class.java)
+
+        client.postAbs("https://spamcheck.postmarkapp.com/filter", { res ->
+            res.bodyHandler {
+                ctx.response()
+                    .setStatusCode(201)
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    .end(it.toJsonObject().encodePrettily())
+            }
+        })
+            .putHeader("content-type","application/json; charset=utf-8")
+            .setChunked(true)
+            .end(Json.encode(mail))
+    }
+
     override fun start() {
-        // your code goes here...
-        vertx.createHttpServer().requestHandler { req ->
-            req.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!")
-        }.listen(8080)
+
+        val router = Router.router(vertx)
+        val client = vertx.createHttpClient()
+
+        router.route().handler(BodyHandler.create())
+        router.post("/api/check").handler { this.checkScore(it) }
+
+        client.post(RequestOptions(config()))
+
+        vertx.createHttpServer()
+                .requestHandler({ router.accept(it) })
+                .listen(8080, "127.0.0.1") { res ->
+                    if (res.failed())
+                        res.cause().printStackTrace()
+                }
     }
 }
