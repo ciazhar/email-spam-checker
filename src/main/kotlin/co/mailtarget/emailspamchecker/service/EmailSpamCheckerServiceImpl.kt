d813265f744444c27b815e44e38888e9d706edd5
ciazhar <ciazhar.id@gmail.com>
2018-02-19 12:54:52 +0700

    add separated content from 1 until 5 word for each content

--- src/main/kotlin/co/mailtarget/emailspamchecker/service/EmailSpamCheckerServiceImpl.kt
+++ src/main/kotlin/co/mailtarget/emailspamchecker/service/EmailSpamCheckerServiceImpl.kt
@@ -9,7 +9,17 @@ class EmailSpamCheckerServiceImpl : EmailSpamCheckerService {
 
     override fun check(content: String): HashSet<String> {
 
-        val separateContent = content.split("\\s".toRegex()).toHashSet()
+        val splitWord = content.toLowerCase().split(" ")
+        val separateContent = mutableListOf<String>().toHashSet()
+
+        (0 until splitWord.size).mapTo(separateContent) { splitWord[it] }
+        (0 until splitWord.size - 1).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1] }
+        (0 until splitWord.size - 2).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1]+ " " + splitWord[it + 2] }
+        (0 until splitWord.size - 3).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1]+ " " + splitWord[it + 2]+ " " + splitWord[it + 3] }
+        (0 until splitWord.size - 4).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1]+ " " + splitWord[it + 2]+ " " + splitWord[it + 3]+ " " + splitWord[it + 4] }
+
+        print(separateContent)
+
         val triggeredWordList = TriggerWord.english.toHashSet()+
                                 TriggerWord.bahasa.toHashSet()
 
