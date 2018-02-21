8994ed048e63d81acc9ec3a941ef5cd47530ae23
ciazhar <ciazhar.id@gmail.com>
2018-02-21 15:49:15 +0700

    delete print data code

--- src/main/kotlin/co/mailtarget/emailspamchecker/service/EmailSpamCheckerServiceImpl.kt
+++ src/main/kotlin/co/mailtarget/emailspamchecker/service/EmailSpamCheckerServiceImpl.kt
@@ -18,8 +18,6 @@ class EmailSpamCheckerServiceImpl : EmailSpamCheckerService {
         (0 until splitWord.size - 3).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1]+ " " + splitWord[it + 2]+ " " + splitWord[it + 3] }
         (0 until splitWord.size - 4).mapTo(separateContent) { splitWord[it] + " " + splitWord[it + 1]+ " " + splitWord[it + 2]+ " " + splitWord[it + 3]+ " " + splitWord[it + 4] }
 
-        print(separateContent)
-
         val triggeredWordList = TriggerWord.english.toHashSet()+
                                 TriggerWord.bahasa.toHashSet()
 
