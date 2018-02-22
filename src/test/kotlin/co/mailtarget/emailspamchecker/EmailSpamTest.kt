f2a0ab13c798e227a71a3e3e4ebee0a46ca5fd29
ciazhar <ciazhar.id@gmail.com>
2018-02-22 12:07:59 +0700

    update unit testing

--- src/test/kotlin/co/mailtarget/emailspamchecker/EmailSpamTest.kt
+++ src/test/kotlin/co/mailtarget/emailspamchecker/EmailSpamTest.kt
@@ -44,4 +44,46 @@ class EmailSpamTest {
         val result = EmailSpamChecker.checkSpamScore(str)
         print(result)
     }
+
+    val additionalSpamWord = mutableListOf(
+            "im",
+            "mister",
+            "mobaloba"
+    ).toHashSet()
+
+    @Test
+    fun checkSpamWithAdditionalSpamWordListAndMustReturnHashSetWithNoSpamWord(){
+        val str = "Kotlination.com = Be Kotlineer - Be Simple - Be Connective"
+        val result = EmailSpamChecker.checkSpamAndReturnHashSet(str,additionalSpamWord)
+        Assert.assertTrue(result.isEmpty())
+    }
+
+    @Test
+    fun checkSpamWithAdditionalSpamWordListAndMustReturnHashSetWithSpamWord(){
+        val str = "Free Mobaloba"
+        val result = EmailSpamChecker.checkSpamAndReturnHashSet(str,additionalSpamWord)
+        Assert.assertTrue(!result.isEmpty())
+    }
+
+
+    @Test
+    fun checkSpamWithAdditionalSpamWordListAndMustReturnTrue(){
+        val str = "im Mister Mobaloba 100% free viagra with Vitamin C amazed amazing buy cancel"
+        val result = EmailSpamChecker.isSpam(str,additionalSpamWord)
+        Assert.assertEquals(true,result)
+    }
+
+    @Test
+    fun checkSpamWithAdditionalSpamWordListAndMustReturnFalse() {
+        val str = "Kotlination.com = Be Kotlineer - Be Simple - Be Connective"
+        val result = EmailSpamChecker.isSpam(str,additionalSpamWord)
+        Assert.assertEquals(false,result)
+    }
+
+    @Test
+    fun checkSpamScoreWithAdditionalSpamWordList() {
+        val str = "100% Vitamin C viagra Mobaloba"
+        val result = EmailSpamChecker.checkSpamScore(str,additionalSpamWord)
+        print(result)
+    }
 }
